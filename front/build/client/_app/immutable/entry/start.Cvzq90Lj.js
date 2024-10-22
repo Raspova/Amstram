0fe37165d7b1438520849912df43869d78317892
jon <jonoelhen@gmail.com>
2024-10-23 00:23:45 +0200

    [add] build image

---
+++ front/build/client/_app/immutable/entry/start.Cvzq90Lj.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DzRLttJJ.js";export{t as start};
--- front/build/server/index.js
+++ front/build/server/index.js
@@ -178,7 +178,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "146hryz"
+  version_hash: "rd1qhl"
 };
 async function get_hooks() {
   return {};
